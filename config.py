4d4ca3e58f6ed33c6c011cc01dfd84b26d381237
nazariipenin <nazarii.penin.25@kse.org.ua>
2025-12-07 23:24:50 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# We use this config for tests and visualisation
+# Repeat the process to calculate the average time and what method is better for you
+# You can change this values
+
+#For graph:
+num_nodes = 10
+probability = 0.1
+directed: True / False
+
+#Bencmark settings:
+sizes = [20, 50, 100, 150, 200]
+
+#Visualisation:
+show_matrix_plot: True / False
+show_graph_plot: True / False
+show_benchmark_plot: True/False
